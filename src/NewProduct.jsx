0e79068b6bf7abcfb2e6c10fcf4263bcf456fa1e
craigmarc <cmarcinkiewicz2000@yahoo.com>
2024-12-16 17:02:15 -0500

    got inputs working correctly

--- src/NewProduct.jsx
+++ src/NewProduct.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import Header from './Header'
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 
 const NewProduct = (props) => {
 
@@ -12,68 +12,20 @@ const NewProduct = (props) => {
 
   } = props;
 
- //  think can get rid of picarray    ***************
+  //  think can get rid of picarray    ***************
   const [picArray, setPicArray] = useState([])
   const [current_data, setCurrent_data] = useState()
   const [sizeArray, setSizeArray] = useState([])
+  const displayNew = useRef(true);
+  const displayColor = useRef(false);
+  
+
 
   const token = sessionStorage.getItem("token");
   const tokenOb = JSON.parse(token)
   const tokenFetch = `Bearer ${tokenOb.token}`
 
-  //submit new post
-  /*
-    const handleSubmit = async e => {
-      e.preventDefault();
-      const data = Object.fromEntries(new FormData(e.target).entries());
-  
-      const formData = new FormData();
-  
-      formData.append("title", data.title)
-      formData.append("category", data.category)
-      formData.append("brand", data.brand)
-      formData.append("color", data.color)
-      formData.append("description", data.description)
-      formData.append("modelNum", data.modelNum)
-      formData.append("price", Math.round(data.price * 100))
-      formData.append("length", Math.round(data.length * 100))
-      formData.append("height", Math.round(data.height * 100))
-      formData.append("width", Math.round(data.width * 100))
-      formData.append("weight", Math.round(data.weight * 100));
-      formData.append("quantity", data.quantity)
-      formData.append("image", data.image);
-  
-  
-      await fetch("http://localhost:3000/products/new", {
-  
-        method: 'Post',
-        body: formData,
-  
-        headers: {
-          Authorization: tokenFetch,
-          //'Content-type': 'application/json; charset=UTF-8',
-  
-        },
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          navigate('/');
-  
-        })
-        .catch((err) => {
-          console.log(err.message);
-  
-          if (err.message.includes("Unauthorized")) {
-            sessionStorage.removeItem("token");
-            sessionStorage.removeItem("user_id");
-            sessionStorage.removeItem("message");
-            setLogMessage(true)
-            navigate('/login')
-          }
-  
-        });
-  
-    }*/
+ 
 
   // submit new product
 
@@ -105,10 +57,13 @@ const NewProduct = (props) => {
 
       .then((response) => response.json())
       .then((data) => {
-       
+
         setCurrent_data(data)
+        displayNew.current = false
+        displayColor.current = true
+
         //e.target.reset()
-       
+
       })
 
 
@@ -135,24 +90,27 @@ const NewProduct = (props) => {
     const idData = { ...data, id_size: uuid }
     const newData = [...sizeArray, idData]
     setSizeArray(newData)
-
+    let allInputs = document.querySelectorAll('.sizeInput');
+    allInputs.forEach(singleInput => singleInput.value = '');
+    //setDisplaySize(false)
   }
-console.log(sizeArray)
-    //submit new color array
 
-const submitProduct = async event => {
-  
+
+  //submit new color array
+
+  const submitProduct = async event => {
+
     event.preventDefault();
     const data = Object.fromEntries(new FormData(event.target).entries());
     let uuid = self.crypto.randomUUID();
     const idData = { ...data, sizeArray: sizeArray, id_product: uuid }
-   
+
 
     await fetch("http://localhost:3000/products/product_array", {
       method: 'PUT',
       body: JSON.stringify({
-      color_array: idData,
-      current_id: current_data._id
+        color_array: idData,
+        current_id: current_data._id
 
       }),
       headers: {
@@ -165,10 +123,10 @@ const submitProduct = async event => {
 
       .then((response) => response.json())
       .then((data) => {
-        console.log(data)
+
         setCurrent_data(data)
-        //e.target.reset()
-       
+        setSizeArray([])
+
       })
 
 
@@ -185,7 +143,7 @@ const submitProduct = async event => {
       });
 
 
-    
+
 
     clearAllInputs()
   }
@@ -199,11 +157,11 @@ const submitProduct = async event => {
 
   // add pic to color array
 
-  const newImage = async (e )=> {
+  const newImage = async (e) => {
     e.preventDefault();
     const data = Object.fromEntries(new FormData(e.target).entries());
     console.log(e.target.id)
-    
+
     const formData = new FormData();
 
 
@@ -247,58 +205,13 @@ const submitProduct = async event => {
       });
 
   }
-  
+
 
   function handleDelete() {
 
   }
 
-  // show colors arrray
 
-  function showProducts() {
-if (current_data) {
-    if (current_data.colorArray) {
-    return (
-      <div>
-        {current_data.colorArray.map((index, iter) => {
-
-          return (
-
-            <div key={index.id_product} className="productCard">
-              <p>{index.color}</p>
-              <p>{index.size}</p>
-              <p>{index.price}</p>
-              <p>{index.quantity}</p>
-              <p>{index.length}</p>
-              <p>{index.width}</p>
-              <p>{index.height}</p>
-              <p>{index.weight}</p>
-
-            
-               <div className="addImageContainer">
-          <form encType="multipart/form-data" id={iter} onSubmit={newImage}>
-            <label>
-              <div className="form-group">
-                <label>Image (file must be .jpeg .jpg or .png):</label>
-                <input type="file" className="form-control-file" id="image" name="image" accept=".jpeg, .jpg, .png" />
-              </div>
-            </label>
-            <div className="addImage">
-              <button type="submit">Add New Picture</button>
-            </div>
-          </form>
-        </div>
-              <button className="delete" value={index._id} onClick={handleDelete} >delete product</button>
-            </div>
-
-          )
-        })}
-      </div>
-
-    )
-  }
-}
-  }
 
   // create size array
 
@@ -309,37 +222,37 @@ if (current_data) {
         <form onSubmit={submitSize}>
           <label>
             <p>Size</p>
-            <input className="productInput" type="text" name="size" />
+            <input className="sizeInput" type="text" name="size" />
           </label>
           <label>
             <p>Price</p>
-            <input className="productInput" type="number" step="0.01" name="price" required />
+            <input className="sizeInput" type="number" step="0.01" name="price" required />
           </label>
           <label>
             <p>Quantity</p>
-            <input className="productInput" type="number" step="0.01" name="quantity" required />
+            <input className="sizeInput" type="number" step="0.01" name="quantity" required />
           </label>
           <label>
             <p>Length</p>
-            <input className="productInput" type="number" step="0.01" name="length" required />
+            <input className="sizeInput" type="number" step="0.01" name="length" required />
           </label>
           <label>
             <p>Width</p>
-            <input className="productInput" type="number" step="0.01" name="width" required />
+            <input className="sizeInput" type="number" step="0.01" name="width" required />
           </label>
           <label>
             <p>Height</p>
-            <input className="productInput" type="number" step="0.01" name="height" required />
+            <input className="sizeInput" type="number" step="0.01" name="height" required />
           </label>
           <label>
             <p>Weight</p>
-            <input className="productInput" type="number" name="weight" required />
+            <input className="sizeInput" type="number" name="weight" required />
           </label>
           <div className="newProductSubmit">
             <button type="submit product">Add Size</button>
           </div>
         </form>
-       
+
 
 
       </div>
@@ -357,10 +270,152 @@ if (current_data) {
         <form onSubmit={submitProduct}>
           <label>
             <p>Color</p>
-            <input className="productInput" type="text" name="color" />
+            <input className="productInput" type="text" name="color" required />
           </label>
           <div className="newProductSubmit">
-            <button type="submit product">Add Color</button>
+
+            <button type="submit product">Submit</button>
+          </div>
+        </form>
+        {addSize()}
+      </div>
+    )
+  }
+
+  // show new product
+
+  function showNew() {
+    return (
+      <div>
+        <p>{current_data.title}</p>
+        <p>category: {current_data.category}</p>
+        <p>brand: {current_data.brand}</p>
+        <p>modelNum: {current_data.modelNum}</p>
+        <p>description: {current_data.description}</p>
+      </div>
+    )
+  }
+
+   
+
+
+  // show colors arrray
+  
+  function showProducts() {
+    if (current_data) {
+      if (current_data.colorArray) {
+        return (
+          <div>
+            {current_data.colorArray.map((index, iter) => {
+
+              return (
+
+                <div key={index.id_product} className="productCard">
+                  <p>{index.color}</p>
+
+                  {index.sizeArray.map((index, iter) => {
+                    return (
+                      <div key={iter}>
+                        <p>size: {index.size}</p>
+                        <p>price: {index.price}</p>
+                        <p>quantity: {index.quantity}</p>
+                        <p>length: {index.length}</p>
+                        <p>width: {index.width}</p>
+                        <p>height: {index.height}</p>
+                        <p>weight: {index.weight}</p>
+
+                      </div>
+                    )
+                  })}
+                
+
+                  <div className="addImageContainer">
+                    <form encType="multipart/form-data" id={iter} onSubmit={newImage}>
+                      <label>
+                        <div className="form-group">
+                          <label>Image (file must be .jpeg .jpg or .png):</label>
+                          <input type="file" className="form-control-file" id="image" name="image" accept=".jpeg, .jpg, .png" />
+                        </div>
+                      </label>
+                      <div className="addImage">
+                        <button type="submit">Add New Picture</button>
+                      </div>
+                    </form>
+                  </div>
+                  <button className="delete" value={index._id} onClick={handleDelete} >delete product</button>
+                </div>
+
+              )
+            })}
+          </div>
+
+        )
+      }
+    }
+  }
+ 
+
+  // display main form
+
+  function displayForms() {
+    if (displayNew.current == true) {
+      return (
+        <div>
+          {displayMain()}
+        </div>
+      )
+    }
+    else {
+      return (
+        <div>
+          {showNew()}
+        </div>
+      )
+    }
+  }
+
+  // display colorform
+
+  function displayColorForm() {
+    if (displayNew.current == false && displayColor.current == true) {
+      return (
+        <div>
+          {addProduct()}
+        </div>
+      )
+    }
+
+  }
+
+  
+
+
+  function displayMain() {
+    return (
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>
+            <p>Product Name</p>
+            <input className="titleInput" type="text" name="title" required />
+          </label>
+          <label>
+            <p>Category</p>
+            <input className="titleInput" type="text" name="category" required />
+          </label>
+          <label>
+            <p>Brand</p>
+            <input className="titleInput" type="text" name="brand" />
+          </label>
+          <label>
+            <p>Description</p>
+            <textarea className="descriptInput" type="text" name="description" required />
+          </label>
+          <label>
+            <p>Model Number</p>
+            <input className="titleInput" type="text" name="modelNum" />
+          </label>
+          <div className="newProductSubmit">
+            <button type="submit product">Submit</button>
           </div>
         </form>
       </div>
@@ -375,34 +430,10 @@ if (current_data) {
         setLogMessage={setLogMessage}
       />
       <h2 className="pageTitle">New Product</h2>
-      <form onSubmit={handleSubmit}>
-        <label>
-          <p>Product Name</p>
-          <input className="titleInput" type="text" name="title" required />
-        </label>
-        <label>
-          <p>Category</p>
-          <input className="titleInput" type="text" name="category" required />
-        </label>
-        <label>
-          <p>Brand</p>
-          <input className="titleInput" type="text" name="brand" />
-        </label>
-        <label>
-          <p>Description</p>
-          <textarea className="descriptInput" type="text" name="description" required />
-        </label>
-        <label>
-          <p>Model Number</p>
-          <input className="titleInput" type="text" name="modelNum" />
-        </label>
-        <div className="newProductSubmit">
-          <button type="submit product">Submit</button>
-        </div>
-      </form>
+      {displayForms()}
       {showProducts()}
-      {addProduct()}
-      {addSize()}
+      {displayColorForm()}
+
     </div>
   )
 }
